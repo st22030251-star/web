6cdbbf2a84574bc593f0cbee945d829c410903f2
eldelosnamesxd-create <eldelosnamesxd@gmail.com>
2025-11-06 18:59:08 -0600

    first commit

--- CASHTRACKR/backend/src/server.ts
+++ CASHTRACKR/backend/src/server.ts
@@ -1,13 +1,21 @@
-import express from 'express' 
-import colors from 'colors'
-import morgan from 'morgan'
+import express from "express";
+import colors from "colors";
+import morgan from "morgan";
+import { db } from "./config/db";
 
-const app = express()
+async function connectDB() {
+  try {
+    await db.authenticate();
+    await db.sync();
+    console.log(colors.blue.bold("Conexion exitosa con la DB"));
+  } catch (error) {
+    console.log(colors.blue.bold("Conexion Fallida"));
+  }
+}
+const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.use(express.json())
+app.use(express.json());
 
-
-
-export default app
+export default app;
